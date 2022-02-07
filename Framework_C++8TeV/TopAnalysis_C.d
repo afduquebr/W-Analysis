da8f5ce89cd66ad04ada0339cf6399e5a3e2c74c
Andrés Duque <andresduquebran@Andress-MacBook-Pro.local>
2022-02-07 18:44:35 -0500

    Initial commit

---
+++ Framework_C++8TeV/TopAnalysis_C.d
@@ -0,0 +1,12 @@
+
+# DO NOT DELETE
+
+./TopAnalysis_C.so: TopAnalysis.h /usr/include/root/TROOT.h
+./TopAnalysis_C.so: /usr/include/root/TChain.h /usr/include/root/TFile.h
+./TopAnalysis_C.so: /usr/include/root/TSelector.h /usr/include/root/TH1.h
+./TopAnalysis_C.so: Tophistograms.h /usr/include/root/TH2.h
+./TopAnalysis_C.so: /usr/include/root/TH3.h /usr/include/root/TStyle.h
+./TopAnalysis_C.so: /usr/include/root/TMath.h
+./TopAnalysis_C.so: /usr/include/root/TLorentzVector.h
+./TopAnalysis_C.so: /usr/include/root/RVersion.h /usr/include/root/RConfig.h /usr/include/root/TClass.h /usr/include/root/TDictAttributeMap.h /usr/include/root/TInterpreter.h /usr/include/root/TROOT.h /usr/include/root/TBuffer.h /usr/include/root/TMemberInspector.h /usr/include/root/TError.h /usr/include/root/RtypesImp.h /usr/include/root/TIsAProxy.h /usr/include/root/TFileMergeInfo.h /usr/include/root/TCollectionProxyInfo.h /usr/bin/rootcling
+TopAnalysis_C__ROOTBUILDVERSION= 6.24/06
